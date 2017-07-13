6dfefa9fbdcbac8f908e7b0feb4e7f80331fcde0
xfornt <xfront@126.com>
2017-07-13 23:07:47 +0800

    20170713

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <!--头部开始-->
     <div class="header-box">
       <div class="header">
         <div class="header-left">
@@ -35,6 +36,7 @@
         </form>
       </div>
     </div>
+    <!--头部结束-->
     <!--导航栏开始-->
     <ul id="nav">
       <li v-for="nav in navlist">
@@ -44,17 +46,21 @@
       </li>
     </ul>
     <!--导航栏结束-->
-    
+
     <div class="videolist">
       <!--幻灯片开始-->
       <div class="thumbnail">
+          <div class="p-taglt">
+            <i class="ico-isvip"></i>
+          </div>
           <div class="thumb-pic">
             <img src="http://r1.ykimg.com/050C000059609755ADC0AE17B902E817" class="thumb-real-pic">
-            <a href="http://m.youku.com" class="thumb-link"></a>
+            <a href="http://m.youku.com" class="p-link"></a>
           </div>
           <div class="p-metadata">
               <div class="p-title"><军师联盟>司马懿柏灵筠生死较量 醋缸上线画风跑偏</div>
               <div class="p-desc">
+                <span class="p-check">更新至41</span>
                 <i class="ico-stat-play"></i>
                 <span class="p-num">52.3亿</span>
                 <i class="ico-stat-comment"></i>
@@ -63,29 +69,91 @@
           </div>
       </div>
       <!--幻灯片结束-->
-      <!--视频列表开始-->
-      <div class="v-item">
-        <img src="http://r1.ykimg.com/0515000059609933ADC0AE0E60013AAE">
-        <div class="v-info">
-          <div class="v-title">[快男]养鸡带假发变身</div>
-          <div class="v-desc">
-            <span class="v-num">播放</span>
-            <span class="v-num">评论</span>
+      <!--推荐视频开始-->
+      <div class="v-recommend">
+        <div class="v-item">
+          <img src="http://r1.ykimg.com/0515000059609933ADC0AE0E60013AAE">
+          <div class="v-info">
+            <div class="v-title">[快男]养鸡带假发变身</div>
+            <div class="v-desc">
+              <span class="v-status">更新至46</span>
+              <i class="ico-stat-play"></i>
+              <span class="v-num">播放</span>
+              <i class="ico-stat-comment"></i>
+              <span class="v-num">评论</span>
+            </div>
+          </div>
+        </div>
+        <div class="v-item">
+          <img src="http://r1.ykimg.com/0515000059609933ADC0AE0E60013AAE">
+          <div class="v-info">
+            <div class="v-title">[快男]养鸡带假发变身</div>
+            <div class="v-desc">
+              <i class="ico-stat-play"></i>
+              <span class="v-num">播放</span>
+              <i class="ico-stat-comment"></i>
+              <span class="v-num">评论</span>
+            </div>
+          </div>
+        </div>
+        <div class="v-item">
+          <img src="http://r1.ykimg.com/0515000059609933ADC0AE0E60013AAE">
+          <div class="v-info">
+            <div class="v-title">[快男]养鸡带假发变身</div>
+            <div class="v-desc">
+              <i class="ico-stat-play"></i>
+              <span class="v-num">播放</span>
+              <i class="ico-stat-comment"></i>
+              <span class="v-num">评论</span>
+            </div>
+          </div>
+        </div>
+        <div class="v-item">
+          <img src="http://r1.ykimg.com/0515000059609933ADC0AE0E60013AAE">
+          <div class="v-info">
+            <div class="v-title">[快男]养鸡带假发变身</div>
+            <div class="v-desc">
+              <i class="ico-stat-play"></i>
+              <span class="v-num">播放</span>
+              <i class="ico-stat-comment"></i>
+              <span class="v-num">评论</span>
+            </div>
+          </div>
+        </div>
+        <div class="v-item">
+          <img src="http://r1.ykimg.com/0515000059609933ADC0AE0E60013AAE">
+          <div class="v-info">
+            <div class="v-title">[快男]养鸡带假发变身</div>
+            <div class="v-desc">
+              <i class="ico-stat-play"></i>
+              <span class="v-num">播放</span>
+              <i class="ico-stat-comment"></i>
+              <span class="v-num">评论</span>
+            </div>
+          </div>
+        </div>
+        <div class="v-item">
+          <img src="http://r1.ykimg.com/0515000059609933ADC0AE0E60013AAE">
+          <div class="v-info">
+            <div class="v-title">[快男]养鸡带假发变身</div>
+            <div class="v-desc">
+              <i class="ico-stat-play"></i>
+              <span class="v-num">播放</span>
+              <i class="ico-stat-comment"></i>
+              <span class="v-num">评论</span>
+            </div>
           </div>
         </div>
       </div>
-      <div class="v-item">
-        <img src="http://r1.ykimg.com/0515000059609933ADC0AE0E60013AAE">
-        <div class="v-info">
-          <div class="v-title">[快男]养鸡带假发变身</div>
-          <div class="v-desc">
-            <i class="ico-stat-play" title="播放"></i>
-            <span class="v-num">播放</span>
-            <span class="v-num">评论</span>
-          </div>
-        </div>
-      </div>
-      <!--视频列表结束-->
+      <!--推荐视频结束-->
+    </div>
+    <!--右下角固定工具栏-->
+    <div class="rsidebar">
+      <ul>
+        <li><a ><i class="ico-gotop"></i></a></li>
+        <li><a><i class="ico-feedback"></i></a></li>
+        <li><a><i class="ico-user"></i></a></li> 
+      </ul>
     </div>
   </div>
 </template>
@@ -178,6 +246,40 @@ a {
   margin: 0 10px;
 }
 
+/*其他小图标*/
+.ico-stat-play,.ico-stat-comment,.ico-isvip {
+  background: url(http://static.youku.com/index/img/mobile/yk@2x.png) no-repeat;
+  background-size:300px 300px;
+  display: inline-block;
+}
+
+.ico-stat-play,.ico-stat-comment{
+  height: 12px;
+  width:12px;
+  vertical-align: middle;
+}
+
+.ico-isvip {
+  width:44px;
+  height: 44px;
+}
+
+.p-metadata i,.p-metadata .p-num{
+  vertical-align: bottom;
+}
+
+.ico-stat-play {
+  background-position: -30px 0;
+}
+
+.ico-stat-comment {
+  background-position: -60px 0;
+}
+
+.ico-isvip {
+  background-position: -60px -60px;
+}
+
 /*header*/
 .header {
   width:100%;
@@ -251,6 +353,7 @@ a {
 /*大图*/
 .thumbnail{
   margin:15px 10px 10px 10px;
+  position: relative;
 }
 
 .thumb-pic{
@@ -260,6 +363,20 @@ a {
 .thumb-real-pic {
   width:100%;
   height: 100%;
+}
+
+.p-link{
+  position: absolute;
+  top:0;
+  left:0;
+  width:100%;
+  height: 100%;
+}
+
+.p-taglt{
+  position: absolute;
+  top:0;
+  left:0;
 }
 
 /*大图媒体信息*/
@@ -273,25 +390,13 @@ a {
   color:#555;
 }
 
-/*其他小图标*/
-.ico-stat-play,.ico-stat-comment {
-  background: url(http://static.youku.com/index/img/mobile/yk@2x.png) no-repeat;
-  background-size:300px 300px;
-  height: 12px;
-  width:12px;
-  display: inline-block;
-}
 
-.p-metadata i,.p-metadata .p-num{
-  vertical-align: bottom;
-}
-
-.ico-stat-play {
-  background-position: -30px 0;
-}
-
-.ico-stat-comment {
-  background-position: -60px 0;
+/*大图媒体信息*/
+.p-check {
+  font-size:12px;
+  position: absolute;
+  bottom:52px;
+  color:#ddd;
 }
 
 .p-num {
@@ -299,6 +404,7 @@ a {
   color:#555;
 }
 
+/*小视频列表*/
 .v-item {
   float:left;
   width: 50%;
@@ -307,13 +413,26 @@ a {
   -webkit-box-sizing:border-box; /* Safari */
   padding-left: 10px;
   padding-right:10px;
+  margin-bottom:15px;
 }
+
 .v-item img{
   max-width: 100%;
+  
 }
 
 .v-info{
   background-color: #ddd;
+  margin-top:-2px;
+  max-width: 100%;
+}
+
+.v-status{
+  font-size: 12px;
+  color:#fff;
+  position: absolute;
+  bottom:10px;
+  left:15px;
 }
 
 .v-title{
@@ -324,5 +443,53 @@ a {
   font-size: 12px;
   color:#909090;
 }
+
+/*右下角固定工具栏*/
+.rsidebar {
+  position: fixed;
+  right:10px;
+  bottom: 50px;
+  background-color: 
+}
+
+.rsidebar li{
+  list-style: none;
+  text-align: center;
+  margin-top:1px;
+}
+.rsidebar li a {
+  display: block; /*inline元素不能设置宽高*/
+  width:50px;
+  height: 50px;
+  background-color: rgba(0,0,0,0.5);
+  border-radius: 5px;
+}
+
+/*sidebar小图标样式*/
+.rsidebar .ico-gotop,.rsidebar .ico-feedback,.rsidebar .ico-user{
+  background: url(http://static.youku.com/index/img/mobile/sidebar@2x.png) no-repeat; 
+  background-size:32px 130px; /*图片尺寸64 260 图片为什么要缩小一倍*/
+  display: inline-block;
+  margin-top:10px;
+}
+
+.rsidebar .ico-gotop {
+  width:24px;
+  height: 26px;
+  background-position: 0 0;
+}
+
+.rsidebar .ico-user {
+  width:26px;
+  height: 26px;
+  background-position: 0 -100px; /*位置定位*/
+}
+
+.rsidebar .ico-feedback {
+  width:22px;
+  height: 24px;
+  background-position: 0 -40px;
+}
+
 </style>
 
